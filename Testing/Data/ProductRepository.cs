0f334200d360275a0ff359708ab06ccee5390c73
Robert <broncofan3124@msn.com>
2024-09-26 15:28:02 -0700

    Completed

--- Testing/Data/ProductRepository.cs
+++ Testing/Data/ProductRepository.cs
@@ -18,4 +18,40 @@ public class ProductRepository : IProductRepository
     {
         return _connection.Query<Product>("SELECT * FROM products;");
     }
+    
+    public Product GetProduct(int id)
+    {
+        return _connection.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+    }
+    
+    public void UpdateProduct(Product product)
+    {
+        _connection.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+            new {name = product.Name, price = product.Price, id = product.ProductID });
+    }
+    
+    public void InsertProduct(Product productToInsert)
+    {
+        _connection.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+            new {name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+    }
+    public IEnumerable<Category> GetCategories()
+    {
+        return _connection.Query<Category>("SELECT * FROM categories;");
+    }
+    
+    public Product AssignCategory()
+    {
+        var categoryList = GetCategories();
+        var product = new Product();
+        product.Categories = categoryList;
+        return product;
+    }
+    
+    public void DeleteProduct(Product product)
+    {
+        _connection.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new {id = product.ProductID });
+        _connection.Execute("DELETE FROM Sales WHERE ProductID = @id;",  new { id = product.ProductID });
+        _connection.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
+    }
 }
